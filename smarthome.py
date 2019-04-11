14d6d1336282f55637411f85778ebec6cc290caf
DewGew <pierlar@hotmail.com>
2019-04-11 17:28:01 +0200

    Update smarthome.py

--- smarthome.py
+++ smarthome.py
@@ -54,9 +54,9 @@ def AogGetDomain(device):
             return lightDOMAIN
         elif device["Image"] in IMAGE_MEDIA:
             return mediaDOMAIN
-    elif 'Lighting 1' == device["Type"]
+    elif 'Lighting 1' == device["Type"]:
         return lightDOMAIN
-    elif 'Lighting 2' == device["Type"]
+    elif 'Lighting 2' == device["Type"]:
         return lightDOMAIN
     elif 'Group' == device["Type"]:
         return groupDOMAIN
