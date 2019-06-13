ec5fef178778673a46ea98c228519c1440cf580c
DewGew <pierlar@hotmail.com>
2019-06-13 11:40:14 +0200

    Added states

--- helpers.py
+++ helpers.py
@@ -15,3 +15,22 @@ class SmartHomeErrorNoChallenge(Exception):
         super().__init__(msg)
         self.code = code 
         self.desc = desc
+ 
+class AogState:
+    def __init__(self):
+        self.state = ''
+        self.domain = ''
+        self.attributes = 0
+        self.name = ''
+        self.entity_id = ''
+        self.id = ''
+        self.nicknames = None
+        self.room = None
+        self.level = 0
+        self.temp = 0
+        self.humidity = 0
+        self.setpoint = ''
+        self.color = ''
+        self.protected = None
+        self.maxdimlevel = 0
+        self.seccode = ''
