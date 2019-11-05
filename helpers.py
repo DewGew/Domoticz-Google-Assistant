7d5dc99b2d4d7324150b3e0031111133b56ff5b2
DewGew <pierlar@hotmail.com>
2019-11-05 14:19:15 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,8 @@
 """Helper classes for Google Assistant integration."""
 
+import os
+from const import FILE_DIR
+
 class SmartHomeError(Exception):
     """Google Assistant Smart Home errors.
     https://developers.google.com/actions/smarthome/create-app#error_responses
@@ -37,3 +40,83 @@ class AogState:
         self.tempunit = None
         self.battery = 0
         self.ack = False
+
+def uptime():
+    try:
+        f = open( "/proc/uptime" )
+        contents = f.read().split()
+        f.close()
+    except:
+        return "Cannot open uptime file: /proc/uptime"
+
+    total_seconds = float(contents[0])
+
+    # Helper vars:
+    MINUTE  = 60
+    HOUR    = MINUTE * 60
+    DAY     = HOUR * 24
+
+    # Get the days, hours, etc:
+    days    = int( total_seconds / DAY )
+    hours   = int( ( total_seconds % DAY ) / HOUR )
+    minutes = int( ( total_seconds % HOUR ) / MINUTE )
+    seconds = int( total_seconds % MINUTE )
+
+    # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
+    string = ""
+    if days > 0:
+        string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+    if len(string) > 0 or hours > 0:
+        string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
+    if len(string) > 0 or minutes > 0:
+        string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
+        string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
+
+    return string;
+
+def proc_starttime(pid):
+    p = re.compile(r"^btime (\d+)$", re.MULTILINE)
+    m = p.search(open("/proc/stat").read())
+    btime = int(m.groups()[0])
+    clk_tck = os.sysconf(os.sysconf_names["SC_CLK_TCK"])
+    stime = int(open("/proc/%d/stat" % pid).read().split()[21]) / clk_tck
+    
+    total_seconds = btime + stime
+
+    # Helper vars:
+    MINUTE  = 60
+    HOUR    = MINUTE * 60
+    DAY     = HOUR * 24
+
+    # Get the days, hours, etc:
+    days    = int( total_seconds / DAY )
+    hours   = int( ( total_seconds % DAY ) / HOUR )
+    minutes = int( ( total_seconds % HOUR ) / MINUTE )
+    seconds = int( total_seconds % MINUTE )
+
+    # Build up the pretty string (like this: "N days, N hours, N minutes, N seconds")
+    string = ""
+    if days > 0:
+        string += str(days) + " " + (days == 1 and "day" or "days" ) + ", "
+    if len(string) > 0 or hours > 0:
+        string += str(hours) + " " + (hours == 1 and "hour" or "hours" ) + ", "
+    if len(string) > 0 or minutes > 0:
+        string += str(minutes) + " " + (minutes == 1 and "minute" or "minutes" ) + ", "
+        string += str(seconds) + " " + (seconds == 1 and "second" or "seconds" )
+
+    return string;
+    
+def readFile(filename):
+    """Read file."""
+    file = open(os.path.join(FILE_DIR, filename), 'r+')
+    code = file.read()
+    file.close()
+    return code
+        
+def saveFile(filename, content):
+    """Read file."""
+    file = open(os.path.join(FILE_DIR, filename), 'w+')
+    code = file.read()
+    file.write(content)
+    file.close()
+    return code
