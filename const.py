8e0ed7ed6c1028179910160a1449a768162640ce
DewGew <pierlar@hotmail.com>
2019-11-29 07:59:37 +0100

    Add logtofile info in UI

--- const.py
+++ const.py
@@ -358,6 +358,7 @@ TEMPLATE = """
             
             <p><b>port_settings:</b><br>Set the local port. Default is <code>port_number: 3030</code></p>
             <p><b>loglevel:</b><br>Set log level <code>Debug</code>, <code>Info</code> or <code>Error</code>. Default is <code>Info</code></p> 
+            <p><b>logtofile:</b><br>Enable or disable write log to file. If 'false' logs will not show in the LOG tab.</p>
             <p><b>userinterface:</b><br>Enable or disable UI</p>
             <p><b>ngrok_tunnel:</b><br>Use Ngrok tunnel true or false. Instantly create a public HTTPS URL.<br>Don't have to open any port on router and do not require a reverse proxy.<br><b>NOTE:</b>Ngrok assigns random urls. When server restart the server gets a new url</p>                   
             <p><b>auth_user/auth_pass:</b><br>Set the authorization username and password.</p>
