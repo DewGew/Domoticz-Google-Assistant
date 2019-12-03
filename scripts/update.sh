e95fbef56159b2d9518ea9dbc7b01fb9ae4d459e
DewGew <pierlar@hotmail.com>
2019-12-03 15:00:17 +0100

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,7 +12,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -o errexit
 
+scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
+GIT_DIR="$(realpath $(dirname ${BASH_SOURCE[0]})/..)"
+
+# make sure we're running as the owner of the checkout directory
+RUN_AS="$(ls -ld "$scripts_dir" | awk 'NR==1 {print $3}')"
+if [ "$USER" != "$RUN_AS" ]
+then
+    echo ""
+    echo " This script must run as $RUN_AS, trying to change user..."
+    exec sudo -u $RUN_AS $0
+fi
 cd /home/${USER}/
 
 echo "!-----------------------------------!"
