7a85510ee1f1330bce8f4a69b00fcebe402267b1
DewGew <pierlar@hotmail.com>
2020-03-24 15:58:17 +0100

    Update trait.py

--- trait.py
+++ trait.py
@@ -336,7 +336,7 @@ class OpenCloseTrait(_Trait):
         if features & ATTRS_PERCENTAGE:
             response['openPercent'] = self.state.level
         else:
-            if self.state.state == 'Open':
+            if self.state.state in ['Open', 'On']:
                 response['openPercent'] = 100
             else:
                 response['openPercent'] = 0
