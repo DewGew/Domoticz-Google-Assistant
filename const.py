c9ed1d2564a83aed2a6a417ef0ca6fd205ae5bcb
DewGew <pierlar@hotmail.com>
2020-04-01 10:36:27 +0200

    V.1.7.4

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
                     
 """Constants for Google Assistant."""
-VERSION = '1.7.3'
+VERSION = '1.7.4'
 PUBLIC_URL = 'https://[your public url]'
 CONFIGFILE = 'config/config.yaml'
 LOGFILE = 'dzga.log'
