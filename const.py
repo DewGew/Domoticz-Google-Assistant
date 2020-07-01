d9923a887b2fd3c4dfc37b41226ca19d9d762bdb
DewGew <pierre@kapima.se>
2020-07-01 08:11:24 +0200

    [Skip travis] New Version 1.8.5
    This version fixes devicetype function issues
    Added switches and blinds to above function

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
                     
 """Constants for Google Assistant."""
-VERSION = '1.8.3'
+VERSION = '1.8.5'
 PUBLIC_URL = 'https://[your public url]'
 CONFIGFILE = 'config/config.yaml'
 LOGFILE = 'dzga.log'
