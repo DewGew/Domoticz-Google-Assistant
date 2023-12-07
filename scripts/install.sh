e51b8af033b0dfd677e325064888479293e924b2
DewGew <pierlar@hotmail.com>
2023-12-07 15:02:01 +0100

    Update install.sh to work with bookwoorm

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,13 +32,6 @@ sudo apt-get update -y
 sed 's/#.*//' ${INSTALL_DIR}/requirements/system-requirements.txt | xargs sudo apt-get install -y
 cd /home/${USER}/
 
-VER=$(python3 -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
-if [ "$VER" -lt "35" ]; then
-    echo " Domoticz-Google-Assistant requires python 3.5 or greater"
-	echo ""
-    exit 1
-fi
-
 echo ""
 echo " Create virtual enviroment..."
 echo ""
